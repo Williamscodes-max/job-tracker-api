33a1fa93a4277d9d2afcb81f99c15d85c41daea2
williams alayode <walayode@gmail.com>
2026-03-05 08:46:31 +0100

    Add homepage route

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path, include
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/', include('applications.urls')),  # <- our API endpoints
+# ]
+
 from django.contrib import admin
-from django.urls import path, include
+from django.http import JsonResponse
+from django.urls import path
+
+def home(request):
+    return JsonResponse({
+        "message": "Job Tracker API is running successfully"
+    })
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
-    path('api/', include('applications.urls')),  # <- our API endpoints
 ]
